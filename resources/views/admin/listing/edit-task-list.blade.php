61fa1632eb23d0d747953c733a984aa01d389ad9
gitkanchangit <kanchan.skylabstech@gmail.com>
2024-12-11 16:57:02 +0530

    employee

--- resources/views/admin/listing/edit-task-list.blade.php
+++ resources/views/admin/listing/edit-task-list.blade.php
@@ -6,8 +6,8 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-              Service
-                <small>Add Service</small>
+                Edit Task
+                <small>Edit Task</small>
             </h1>
             @include('admin.common.breadcrumb')
         </section>
@@ -20,93 +20,82 @@
                     <div class="box">
                         <!-- /.box-header -->
                         <!-- form start -->
-                        <form class="form-horizontal" action="{{ url('admin/add-service-master') }}"
-                            id="add_service_master" method="post" name="add_service_master" accept-charset='UTF-8'
+                        <form class="form-horizontal" action="{{ url('admin/update-task-list/' . $task->id) }}"
+                            id="edit_task_list" method="post" name="edit_task_list" accept-charset='UTF-8'
                             enctype="multipart/form-data">
                             {{ csrf_field() }}
                             <div class="box-body">
-
-
                                 <div class="form-group row mt-3">
                                     <label for="exampleInputPassword1"
-                                        class="control-label col-sm-3 fw-bold text-md-end mb-2 mb-md-0">Department</label>
+                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Property Name<span
+                                            class="text-danger">*</span></label>
                                     <div class="col-sm-8">
-
-                                      
-                                        <select name="department_id" class="form-control f-14">
-                                          <option value=""> Select Department </option>
-                                          @foreach ($department as $key => $value)
-                                              <option data-icon-class="icon-star-alt" class="text-capitalize" value="{{ $value->id }}">
-                                                  {{ $value->name  }}
-                                              </option>
-                                          @endforeach
+                                        <select class="form-control f-14" name="property_id"
+                                            id="property_name">
+                                            <option value="">--Select Property--</option>
+                                            @foreach ($property as $item)
+                                                <option value="{{ $item->id }}" class="text-capitalize"
+                                                    {{ $task->property_id == $item->id ? 'selected' : '' }}>
+                                                    {{ $item->name }}
+                                                </option>
+                                            @endforeach
                                         </select>
-                                    </div>
-                                </div>
-                                <div class="form-group row mt-3">
-                                    <label for="exampleInputPassword1"
-                                        class="control-label col-sm-3 fw-bold text-md-end mb-2 mb-md-0">Name<span
-                                            class="text-danger">*</span></label>
-                                    <div class="col-sm-8">
-                                        <input type="text" class="form-control f-14" name="name" id="name"
-                                            value="{{ old('name') }}" placeholder="Enter  Name..">
-                                        @if ($errors->has('name'))
+                                        @if ($errors->has('property_name'))
                                             <span class="error-tag">
-                                                <p>{{ $errors->first('name') }}</p>
+                                                <p>{{ $errors->first('property_name') }}</p>
                                             </span>
                                         @endif
                                     </div>
                                 </div>
-
                                 <div class="form-group row mt-3">
                                     <label for="exampleInputPassword1"
-                                        class="control-label col-sm-3 fw-bold text-md-end mb-2 mb-md-0">Amount<span
+                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Date<span
                                             class="text-danger">*</span></label>
                                     <div class="col-sm-8">
-                                        <input type="text" class="form-control f-14" name="amount" id="amount"
-                                            value="{{ old('amount') }}" placeholder="Enter  amount..">
-                                        @if ($errors->has('amount'))
+                                        <input type="text" class="form-control f-14" name="date" id="date"
+                                            value="{{ $task->date }}" placeholder="Enter Date..">
+                                        @if ($errors->has('date'))
                                             <span class="error-tag">
-                                                <p>{{ $errors->first('amount') }}</p>
+                                                <p>{{ $errors->first('date') }}</p>
                                             </span>
                                         @endif
                                     </div>
                                 </div>
-
                                 <div class="form-group row mt-3">
                                     <label for="exampleInputPassword1"
-                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Description<span
+                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Image<span
                                             class="text-danger">*</span></label>
                                     <div class="col-sm-8">
-                                        <textarea name="description" id="description" class="form-control f-14 error" placeholder="Description..">{{ old('description') }}</textarea>
-                                        @if ($errors->has('description'))
+                                        <input type="file" class="form-control f-14" name="image" id="image" accept=".jpg,.jpeg,.png" />
+                                        @if ($errors->has('image'))
                                             <span class="error-tag">
-                                                <p>{{ $errors->first('description') }}</p>
+                                                <p>{{ $errors->first('image') }}</p>
                                             </span>
                                         @endif
                                     </div>
                                 </div>
-
-
-
                                 <div class="form-group row mt-3">
                                     <label for="exampleInputPassword1"
-                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Service Provider</label>
+                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Task<span
+                                            class="text-danger">*</span></label>
                                     <div class="col-sm-8">
-                                        <select class="form-control f-14" name="service_provider" id="service_provider">
-                                            <option value="Inhouse">Inhouse</option>
-                                            <option value="Vendor">Vendor</option>
-                                        </select>
+                                        <input type="text" class="form-control f-14" name="task" id="task"
+                                            value="{{ $task->task }}" placeholder="Enter Task..">
+                                        @if ($errors->has('task'))
+                                            <span class="error-tag">
+                                                <p>{{ $errors->first('task') }}</p>
+                                            </span>
+                                        @endif
                                     </div>
                                 </div>
-
                                 <div class="form-group row mt-3">
                                     <label for="exampleInputPassword1"
-                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Status</label>
+                                        class="control-label col-sm-3 fw-bold text-md-end mb-2">Status<span
+                                            class="text-danger">*</span></label>
                                     <div class="col-sm-8">
-                                        <select class="form-control f-14" name="status" id="status">
-                                            <option value="Active">Active</option>
-                                            <option value="Inactive">Inactive</option>
+                                        <select class="form-control f-14" name="task_status" id="task_status">
+                                            <option value="Complete">Complete</option>
+                                            <option value="Incomplete" {{ $task->task_status == 'Incomplete' ? 'selected' : '' }}>Incomplete</option>
                                         </select>
                                     </div>
                                 </div>
@@ -115,13 +104,11 @@
                             <div class="box-footer">
                                 <button type="submit" class="btn btn-info text-white f-14 me-2"
                                     id="submitBtn">Submit</button>
-                                <a class="btn btn-danger f-14" href="{{ url('admin/service-master') }}">Cancel</a>
+                                <a class="btn btn-danger f-14" href="{{ url('admin/task-list') }}">Cancel</a>
                             </div>
                             <!-- /.box-footer -->
                         </form>
                     </div>
-                    <!-- /.box -->
-
                     <!-- /.box -->
                 </div>
                 <!--/.col (right) -->
@@ -133,3 +120,4 @@
 @section('validate_script')
     <script type="text/javascript" src="{{ asset('public/backend/dist/js/validate.min.js') }}"></script>
 @endsection
+
